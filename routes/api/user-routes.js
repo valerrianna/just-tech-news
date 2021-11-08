61fcde28ecc60654c0996579380049a84f458ff1
Valerie <valerie.tram.anh.n@gmail.com>
2021-11-08 10:13:47 -0500

    lesson 2 completed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -43,49 +43,35 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      // .then(dbUserData => res.json(dbUserData))
+      .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
 });
 
-router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-    User.findOne({
-      where: {
-        email: req.body.email
-      }
-    }).then(dbUserData => {
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that email address!' });
-        return;
-      }
-  
-      // Verify user
-      const validPassword = dbUserData.checkPassword(req.body.password);
-      if (!validPassword) {
-        res.status(400).json({ message: 'Incorrect password!' });
-        return;
-      }
-      
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-  
-    });  
-});
-
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-  // pass in req.body instead to only update what's passed through
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
-})
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
